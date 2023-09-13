ab04fff51cc3150736c544ec4de41fa8f1052396
umair kifayat <umairkifayat3@gmail.com>
2023-09-13 12:54:57 -0700

    submit-form

--- app.js
+++ app.js
@@ -2,48 +2,50 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const p = document.querySelector("p");
 
-const arr = [""]
-
+const arr = [];
+function render(params) {
+    p.innerHTML = "";
+    for (let i = 0; i < arr.length; i++) {
+        p.innerHTML += `<p>Email: ${arr[i].email}, Password: ${arr[i].password}</p>
+        <button onclick="Edittext(${i})">Edit</button>
+        <button onclick="deletetext(${i})">Delete</button>`
+    }
+}
 function user(e) {
     e.preventDefault();
     p.innerHTML = "";
 
-    // console.log(email.value);
-    // console.log(password.value);
     const add = {
         email: email.value,
         password: password.value,
     }
-    // console.log(obj);
+
     arr.push(add);
 
-
-    // console.log(array);
-    for (let i = 1; i < arr.length; i++) {
-        // console.log(add);
-        p.innerHTML += ` <p>Email: ${arr[i].email}, Password: ${arr[i].password}</P>
-    <button>Edit</button>
-    <button>Delete</button>`
-
+    for (let i = 0; i < arr.length; i++) {
+        p.innerHTML += `<p>Email: ${arr[i].email}, Password: ${arr[i].password}</p>
+        <button onclick="Edittext(${i})">Edit</button>
+        <button onclick="deletetext(${i})">Delete</button>`
     }
+
     email.value = "";
     password.value = "";
 
-
-
 }
 
+function deletetext(index) {
+    p.innerHTML = "";
+    arr.splice(index, 1);
 
+    render()
+}
 
+function Edittext(index) {
+    p.innerHTML = " ";
+    // let uservalue = prompt("enter your edit value");
+    const user = prompt("enter your edit value")
+    arr[index].email = user;
+    arr[index].password = user;
 
-
-
-
-
-
-
-
-
-
-
-
+    render()
+}
